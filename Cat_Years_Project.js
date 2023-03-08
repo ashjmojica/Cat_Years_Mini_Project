e0c9689eed02180ddf5215fd1dac50f35162b8f9
Ashley Mojica <108203578+ashjmojica@users.noreply.github.com>
2023-03-08 16:49:59 -0600

    Add files via upload

---
+++ Cat_Years_Project.js
@@ -0,0 +1,20 @@
+const myAge = '31';
+
+
+const earlyYears = 2;
+
+earlyYears *= 25;
+
+
+const laterYears = myAge - 2;
+
+laterYears *= 4;
+
+
+const myAgeInCatYears = earlyYears + laterYears;
+
+const myName = 'Ashley';
+
+console.log('My name is ' + myName + '. I am ' + myAge + ' years old
+in human years which is ' + myAgeInCatYears + ' years old in
+cat years.');
